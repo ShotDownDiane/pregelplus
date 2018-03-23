8653e426256b04cfda9e01d93c00a5d7876632ac
Junhua Zhang <2414890918@qq.com>
2018-03-23 22:25:43 +0800

    untrack useless files

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /usr/local/hadoop-2.7.4/lib/native/libhdfs.a /usr/java/jdk1.8.0_151/jre/lib/amd64/server/libjvm.so
-
-LIBS :=
-
