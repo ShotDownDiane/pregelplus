65a41aa6af6540a4f84ef98fa589cd016b039c5e
ShotDown <532586242@qq.com>
2018-03-23 20:30:59 -0700

    test graph

--- src/run.cpp
+++ src/run.cpp
@@ -4,7 +4,7 @@ int main(int argc, char* argv[]){
 	init_workers();
 	batch=1;
 	init();
-	pregel_hashmin("/test", "/toyOutput", true);
+	pregel_hashmin("/test2", "/toyOutput", true);
 	worker_finalize();
 	return 0;
 }
